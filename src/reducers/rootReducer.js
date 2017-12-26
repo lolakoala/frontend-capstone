71cd57bc8720e30c8ffe695e46b17a25825196ac
Lola Brenner <amandadbrenner@gmail.com>
2017-12-26 14:01:17 -0700

    set up root reducer

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+// import all reducers here
+
+
+const rootReducer = combineReducers({
+  // list all imported reducers here
+});
+
+export default rootReducer;
