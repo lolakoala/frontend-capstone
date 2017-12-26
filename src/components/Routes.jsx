ee44abac95c26ab6924fa3eb105a793553b85d67
Lola Brenner <amandadbrenner@gmail.com>
2017-12-26 13:42:03 -0700

    set up routes file template

---
+++ src/components/Routes.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+//import all containers
+
+const Routes = () => {
+  return (
+    <div className="routes">
+      {/*routes for container components -- see below as example*/}
+      {/* <Route path="/" component={ HeaderNavContainer }/> */}
+    </div>
+  );
+
+};
+
+export default Routes;
