44fcde1877d70af45846c76142b81ed31706365c
Lola Brenner <amandadbrenner@gmail.com>
2017-12-31 12:18:42 -0700

    add redirect for when city is added

--- src/components/SelectCity/SelectCity.js
+++ src/components/SelectCity/SelectCity.js
@@ -7,20 +7,24 @@ import MenuItem from 'material-ui/MenuItem';
 class SelectCity extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: 1};
+    this.state = {value: ''};
   }
-
-  const { currentUser, addCity } = this.props;
 
   handleChange = (event, index, value) => this.setState({value});
 
   addCity = (event, index, value) => {
-    handleChange(event, index, value);
-    addCity(this.state.value, currentUser);
+    const { currentUser, addCity } = this.props;
+
+    this.handleChange(event, index, value);
+    addCity(value, currentUser);
   }
 
   render() {
-    //if currentUser.location redirect to edit profile
+    //if currentUser.city redirect to edit profile
+    if (this.props.currentUser.city) {
+      return <Redirect to="/editProfile" />;
+    }
+
     return (
       <div>
         <h2>Please select your city.</h2>
