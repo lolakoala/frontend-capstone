00c4b3acd3d8ba584435662411b5e4b402930ca5
Lola Brenner <amandadbrenner@gmail.com>
2017-12-31 09:36:46 -0700

    boiler plate for landing page

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class LandingPage extends Component {
+  
+}
+
+export default LandingPage;
+
+LandingPage.propTypes = {
+
+};
