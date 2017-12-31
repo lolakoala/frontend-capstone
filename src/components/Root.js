c6c65327a347a2a164d48820ff6ac86f2762279e
Lola Brenner <amandadbrenner@gmail.com>
2017-12-31 09:11:32 -0700

    add devtools in root

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Routes from './Routes.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import DevTools from '../containers/DevTools';
 
 export default class Root extends Component {
   render() {
@@ -8,6 +9,8 @@ export default class Root extends Component {
       <BrowserRouter>
         <div>
           <Routes />
+          {/*Below are Redux devtools. Uncomment to use.*/}
+          {/* <DevTools /> */}
         </div>
       </BrowserRouter>
     );
