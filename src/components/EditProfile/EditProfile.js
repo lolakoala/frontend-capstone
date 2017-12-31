9b503fe54d330924c69098899751539db9a1dca3
Lola Brenner <amandadbrenner@gmail.com>
2017-12-31 13:15:55 -0700

    add boilerplate and pseudocode for edit profile component

---
+++ src/components/EditProfile/EditProfile.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import TextField from 'material-ui/TextField';
+import Chip from 'material-ui/Chip';
+
+class EditProfile extends Component {
+
+  componentWillMount() {
+    //get list of challenges from store
+  }
+  //upload photo- raisedbutton
+  //username- textfield from material ui
+  //logo - from persisted component?
+  //about me- textfield from material ui
+  //checkboxes of challenges- chips from material ui
+  //submit button- raisedbutton
+  //clear button- raisedbutton
+  render() {
+    return (<div>
+
+    </div>);
+  }
+}
+
+export default EditProfile;
+
+EditProfile.propTypes = {
+  currentUser: PropTypes.object
+};
