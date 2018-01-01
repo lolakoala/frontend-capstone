9d40285065e60509751464fd40fa16ecb80f8c81
Lola Brenner <amandadbrenner@gmail.com>
2018-01-01 13:00:38 -0700

    restructure persisted into class component to use location object

--- src/components/Persisted/Persisted.js
+++ src/components/Persisted/Persisted.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Avatar, RaisedButton } from 'material-ui';
 
@@ -8,29 +8,34 @@ import { Avatar, RaisedButton } from 'material-ui';
 // if not on LandingPage
 // signout
 
-const Persisted = ({ currentUser }) => {
-  const userKeys = Object.keys(currentUser);
+class Persisted extends Component {
+  render () {
 
-  const userInfo = <div>
-    <Avatar src="https://speakerdata2.s3.amazonaws.com/photo/image/839843/thats-what-she-said-0413-1-lgn.jpg" alt="Your user photo"/>
-    <h3>{currentUser.userName}</h3>
-  </div>;
+    console.log(this.props.location.pathname);
+    const userKeys = Object.keys(this.props.currentUser);
 
-  const panic = () => {
-    // use twilio API to text the mental health hotline
-  };
+    const userInfo = <div>
+      <Avatar src="https://speakerdata2.s3.amazonaws.com/photo/image/839843/thats-what-she-said-0413-1-lgn.jpg" alt="Your user photo"/>
+      <h3>{this.props.currentUser.userName}</h3>
+    </div>;
 
-  return (
-    <div>
-      {userKeys.length > 2 ? userInfo : null}
-      <img src="http://one-call.ca/wp-content/uploads/2013/08/logo.png" alt="logo" />
-      <RaisedButton label="PANIC" onClick={() => panic()} />
-    </div>
-  );
-};
+    const panic = () => {
+      // use twilio API to text the mental health hotline
+    };
+
+    return (
+      <div>
+        {userKeys.length > 2 ? userInfo : null}
+        <img src="http://one-call.ca/wp-content/uploads/2013/08/logo.png" alt="logo" />
+        <RaisedButton label="PANIC" onClick={() => panic()} />
+      </div>
+    );
+  }
+}
 
 export default Persisted;
 
 Persisted.propTypes = {
-  currentUser: PropTypes.object
+  currentUser: PropTypes.object,
+  location: PropTypes.object
 };
