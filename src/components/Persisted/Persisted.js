249a2a047aefefe1313c7de2a3c69d0b1dca96d6
Lola Brenner <amandadbrenner@gmail.com>
2018-01-01 12:31:14 -0700

    add boilerplate for persisted content

---
+++ src/components/Persisted/Persisted.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+// username
+// user photo
+// logo
+// panic button
+// dash button
+// signout
+
+const Persisted = props => {
+
+};
+
+export default Persisted;
+
+Persisted.propTypes = {
+
+};
