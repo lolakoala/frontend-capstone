68da206e573d81fc06de8261df0b05bb5c7dc903
Lola Brenner <amandadbrenner@gmail.com>
2018-01-01 12:47:41 -0700

    add logo and panic button

--- src/components/Persisted/Persisted.js
+++ src/components/Persisted/Persisted.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Avatar from 'material-ui/Avatar';
-
-
-// logo
-// panic button
-
-// if user has completed profile (keys > 2)
-// username
-// user photo
+import { Avatar, RaisedButton } from 'material-ui';
 
 // if not on Dash, CreateProfile, LandingPage, or SelectCity
 // dash button
@@ -17,14 +9,22 @@ import Avatar from 'material-ui/Avatar';
 // signout
 
 const Persisted = ({ currentUser }) => {
+  const userKeys = Object.keys(currentUser);
+
   const userInfo = <div>
-    <Avatar src="https://speakerdata2.s3.amazonaws.com/photo/image/839843/thats-what-she-said-0413-1-lgn.jpg" />
+    <Avatar src="https://speakerdata2.s3.amazonaws.com/photo/image/839843/thats-what-she-said-0413-1-lgn.jpg" alt="Your user photo"/>
     <h3>{currentUser.userName}</h3>
   </div>;
 
+  const panic = () => {
+    // use twilio API to text the mental health hotline
+  };
+
   return (
     <div>
-      
+      <img src="http://one-call.ca/wp-content/uploads/2013/08/logo.png" alt="logo" />
+      <RaisedButton label="PANIC" onClick={() => panic()} />
+      {userKeys.length > 2 ? userInfo : null}
     </div>
   );
 };
