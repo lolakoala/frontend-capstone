beadc5c55b9aa0bb7dfc612e39ecb9b68ba45ff2
Lola Brenner <amandadbrenner@gmail.com>
2018-01-02 08:57:00 -0700

    add nav

--- src/components/Dash/Dash.js
+++ src/components/Dash/Dash.js
@@ -5,10 +5,30 @@ import {
 } from 'material-ui';
 import { Link } from 'react-router-dom';
 
-
+// search professionals section
+// search users section
 
 class Dash extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: ''
+    };
+  }
 
+  render() {
+    return (
+      <div>
+        <div>
+          <Link to="/chat">Chat</Link>
+          <Link to="/preferredProffesionals">My Preferred Proffesionals</Link>
+          <Link to="/buddies">My Buddies</Link>
+          {/* In My Toolbox, should we add ability to add your own resources? */}
+          <Link to="/toolbox">My Toolbox</Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Dash;
