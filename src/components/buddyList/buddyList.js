260d1bb9e6e102b4572d9ae6bdd6fe587aaf49bc
Lola Brenner <amandadbrenner@gmail.com>
2018-01-02 16:51:12 -0700

    add boilerplate for buddy list

---
+++ src/components/buddyList/buddyList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+// stuff from material-ui
+} from 'material-ui';
+
+class BuddyList extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default BuddyList;
+
+BuddyList.propTypes = {
+
+};
