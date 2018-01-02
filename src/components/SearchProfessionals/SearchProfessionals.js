60302764e382c08e284b821cb04e2b62581ba594
Lola Brenner <amandadbrenner@gmail.com>
2018-01-02 14:05:13 -0700

    add submit button to search

--- src/components/SearchProfessionals/SearchProfessionals.js
+++ src/components/SearchProfessionals/SearchProfessionals.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { DropDownMenu, MenuItem } from 'material-ui';
+import { DropDownMenu, MenuItem, RaisedButton } from 'material-ui';
 
 class SearchProfessionals extends Component {
   constructor() {
@@ -24,17 +24,31 @@ class SearchProfessionals extends Component {
   handleSearchTopic = (event, index, value) => this.setState({ searchTopic: value });
 
   render() {
-    const insuranceDropDown = <DropDownMenu value={this.state.searchTopic} onChange={this.handleSearchTopic}>
-      {this.props.insuranceList.map((ins, index) => {
-        return <MenuItem value={ins} primaryText={ins} key={`${ins}${index}`}/>;
-      })}
-    </DropDownMenu>;
+    // const insuranceDropDown = <DropDownMenu value={this.state.searchTopic} onChange={this.handleSearchTopic}>
+    //   {this.props.insuranceList.map((ins, index) => {
+    //     return <MenuItem value={ins} primaryText={ins} key={`${ins}${index}`}/>;
+    //   })}
+    // </DropDownMenu>;
+    //
+    // const specialtyDropDown = <DropDownMenu value={this.state.searchTopic} onChange={this.handleSearchTopic}>
+    //   {this.props.specialtyList.map((specialty, index) => {
+    //     return <MenuItem value={specialty} primaryText={specialty} key={`${specialty}${index}`}/>;
+    //   })}
+    // </DropDownMenu>;
 
-    const specialtyDropDown = <DropDownMenu value={this.state.searchTopic} onChange={this.handleSearchTopic}>
-      {this.props.specialtyList.map((specialty, index) => {
-        return <MenuItem value={specialty} primaryText={specialty} key={`${specialty}${index}`}/>;
-      })}
-    </DropDownMenu>;
+    const topicDropDown = list => {
+      return <DropDownMenu value={this.state.searchTopic} onChange={this.handleSearchTopic}>
+        {list.map((elem, index) => {
+          return <MenuItem value={elem} primaryText={elem} key={`${elem}${index}`}/>;
+        })}
+      </DropDownMenu>;
+    };
+
+    const submitButton = <RaisedButton label="Submit" onClick={() => this.props.search({
+      group: 'professionals',
+      query: this.state.searchQuery,
+      topic: this.state.searchTopic
+    })} />;
 
     return (
       <div>
@@ -43,8 +57,8 @@ class SearchProfessionals extends Component {
           <MenuItem value="insurance" primaryText="Search by insurance." />
           <MenuItem value="specialty" primaryText="Search by specialty." />
         </DropDownMenu>
-        {this.state.value === 'insurance' ? insuranceDropDown : null}
-        {this.state.value === 'specialty' ? specialtyDropDown : null}
+        {this.state.value === 'insurance' ? topicDropDown(this.props.insuranceList) : null}
+        {this.state.value === 'specialty' ? topicDropDown(this.props.specialtyList) : null}
         {this.state.searchTopic ? submitButton : null}
       </div>
     );
@@ -57,5 +71,6 @@ SearchProfessionals.propTypes = {
   getInsuranceList: PropTypes.func,
   getSpecialtyList: PropTypes.func,
   insuranceList: PropTypes.array,
-  specialtyList: PropTypes.array
+  specialtyList: PropTypes.array,
+  search: PropTypes.func
 };
