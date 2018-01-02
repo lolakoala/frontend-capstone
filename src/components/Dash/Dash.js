00b04cc0644372cc32cd911858dad9d6779b397a
Lola Brenner <amandadbrenner@gmail.com>
2018-01-02 15:00:14 -0700

    add boilerplate for searchbuddies

--- src/components/Dash/Dash.js
+++ src/components/Dash/Dash.js
@@ -48,43 +48,6 @@ class Dash extends Component {
   }
 }
 
-// const Dash = ({
-//   getInsuranceList,
-//   getSpecialtyList,
-//   insuranceList,
-//   specialtyList,
-//   search,
-//   searchResults
-// }) => {
-//   return (
-//     <div>
-//       <Toolbar>
-//         <Link to="/chat">Chat</Link>
-//         <Link to="/preferredProffesionals">My Preferred Proffesionals</Link>
-//         <Link to="/buddies">My Buddies</Link>
-//         {/* In My Toolbox, should we add ability to add your own resources? */}
-//         <Link to="/toolbox">My Toolbox</Link>
-//       </Toolbar>
-//       <Tabs>
-//         <Tab label="Search Professionals">
-//           <SearchProfessionals
-//             getInsuranceList={getInsuranceList}
-//             getSpecialtyList={getSpecialtyList}
-//             insuranceList={insuranceList}
-//             specialtyList={specialtyList}
-//             search={search}
-//             searchResults={searchResults}
-//           />
-//         </Tab>
-//         <Tab label="Search Buddies">
-//           {/* Search Users Component Here */}
-//           <div>users</div>
-//         </Tab>
-//       </Tabs>
-//     </div>
-//   );
-// };
-
 export default Dash;
 
 Dash.propTypes = {
---
+++ src/components/SearchBuddies/SearchBuddies.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { DropDownMenu, MenuItem, RaisedButton } from 'material-ui';
+import { Redirect } from 'react-router-dom';
+
+class SearchBuddies extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+}
+
+export default SearchBuddies;
+
+SearchBuddies.propTypes = {
+
+};
