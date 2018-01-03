0d07f1d68b2448cb3fdfd0359c1393a1c61d2c75
Lola Brenner <amandadbrenner@gmail.com>
2018-01-03 16:52:46 -0700

    update reducers from toggle fave action

--- src/reducers/userBuddies.js
+++ src/reducers/userBuddies.js
@@ -2,6 +2,8 @@ const userBuddies = (state = [], action) => {
   switch (action.type) {
   case 'GET_BUDDIES':
     return action.buddies;
+  case 'ADD_BUDDY':
+    return [...state, action.personObject];
   default:
     return state;
   }
--- src/reducers/userProfs.js
+++ src/reducers/userProfs.js
@@ -2,6 +2,8 @@ const userProfs = (state = [], action) => {
   switch (action.type) {
   case 'GET_PREFERRED_PROFS':
     return action.profs;
+  case 'ADD_PROF':
+    return [...state, action.personObject];
   default:
     return state;
   }
