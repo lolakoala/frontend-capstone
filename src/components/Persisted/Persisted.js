eede90b5c203ecf035857e2d13797a568475fb86
Lola Brenner <amandadbrenner@gmail.com>
2018-01-03 20:39:15 -0700

    style buttons in persisted

--- src/components/Persisted/Persisted.js
+++ src/components/Persisted/Persisted.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Avatar, RaisedButton } from 'material-ui';
-import { Link } from 'react-router-dom';
 import css from './Persisted.css';
 import logo from '../../assets/logo1.png';
 
@@ -15,15 +14,33 @@ class Persisted extends Component {
     const { pathname } = this.props.location;
     const userKeys = Object.keys(currentUser);
 
-    const userInfo = <div>
+    const userInfo = <div id="current-user">
       {/* placeholder avatar, should fetch from server?  */}
       <Avatar src="https://speakerdata2.s3.amazonaws.com/photo/image/839843/thats-what-she-said-0413-1-lgn.jpg" alt="Your user photo"/>
       <h3>{currentUser.userName}</h3>
     </div>;
 
-    const signOutButton = <RaisedButton label="Sign Out" onClick={signOut} />;
+    const signOutButton = <RaisedButton
+      label="Sign Out"
+      onClick={signOut}
+      style={{
+        position: 'fixed',
+        top: '70px',
+        right: '25%'
+      }}
+    />;
 
-    const dashLink = <Link to='/dash'>Home</Link>;
+    const dashLink = <RaisedButton
+      label="Home"
+      id="home-button"
+      linkButton={true}
+      href="/dash"
+      style={{
+        position: 'fixed',
+        top: '120px',
+        right: '25%'
+      }}
+    />;
 
     const pathArray = ['/dash', '/', '/createProfile', '/selectCity'];
 
@@ -40,10 +57,9 @@ class Persisted extends Component {
           }}
         />
         {userKeys.length > 2 ? userInfo : null}
-        {/* placeholder logo */}
-        <img src={logo} alt="logo" />
-        {!pathArray.includes(pathname) ? dashLink : null}
+        <img src={logo} alt="logo" id="logo"/>
         {pathname !== '/' ? signOutButton : null}
+        {!pathArray.includes(pathname) ? dashLink : null}
       </div>
     );
   }
