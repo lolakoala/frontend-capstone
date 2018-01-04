2f0dfd7dec3411bcbb5c30d638ef156dd47dfa82
Lola Brenner <amandadbrenner@gmail.com>
2018-01-04 08:29:43 -0700

    style links and subheader

--- src/components/Toolbox/Toolbox.js
+++ src/components/Toolbox/Toolbox.js
@@ -2,6 +2,7 @@ import React from 'react';
 // don't think it needs props yet?
 // import PropTypes from 'prop-types';
 import icon from '../../assets/snail-shell.svg';
+import css from './Toolbox.css';
 import {
   List,
   ListItem,
@@ -10,26 +11,50 @@ import {
 
 const Toolbox = () => {
   return (<List>
-    <Subheader>Mental Health Resources</Subheader>
+    <Subheader style={{
+      fontSize: '30px',
+      fontWeight: 'bold',
+      color: '#253031'
+    }}>Mental Health Resources</Subheader>
     <ListItem
-      primaryText={<a href="https://www.w3schools.com" target="_blank" rel="noopener noreferrer">Visit W3Schools.com!</a>}
+      primaryText={<a href="https://www.w3schools.com" className="toolbox-link" target="_blank" rel="noopener noreferrer">Visit W3Schools.com!</a>}
       leftAvatar={<img src={icon} alt="snail shell icon" height="40" width="40" />}
+      style={{
+        fontSize: '18px',
+        fontWeight: 'bold'
+      }}
     />
     <ListItem
-      primaryText=<a href="https://www.w3schools.com" target="_blank" rel="noopener noreferrer">Visit W3Schools.com!</a>
+      primaryText={<a href="https://www.w3schools.com" className="toolbox-link" target="_blank" rel="noopener noreferrer">Visit W3Schools.com!</a>}
       leftAvatar={<img src={icon} alt="snail shell icon" height="40" width="40" />}
+      style={{
+        fontSize: '18px',
+        fontWeight: 'bold'
+      }}
     />
     <ListItem
-      primaryText=<a href="https://www.w3schools.com" target="_blank" rel="noopener noreferrer">Visit W3Schools.com!</a>
+      primaryText={<a href="https://www.w3schools.com" className="toolbox-link" target="_blank" rel="noopener noreferrer">Visit W3Schools.com!</a>}
       leftAvatar={<img src={icon} alt="snail shell icon" height="40" width="40" />}
+      style={{
+        fontSize: '18px',
+        fontWeight: 'bold'
+      }}
     />
     <ListItem
-      primaryText=<a href="https://www.w3schools.com" target="_blank" rel="noopener noreferrer">Visit W3Schools.com!</a>
+      primaryText={<a href="https://www.w3schools.com" className="toolbox-link" target="_blank" rel="noopener noreferrer">Visit W3Schools.com!</a>}
       leftAvatar={<img src={icon} alt="snail shell icon" height="40" width="40" />}
+      style={{
+        fontSize: '18px',
+        fontWeight: 'bold'
+      }}
     />
     <ListItem
-      primaryText=<a href="https://www.w3schools.com" target="_blank" rel="noopener noreferrer">Visit W3Schools.com!</a>
+      primaryText={<a href="https://www.w3schools.com" className="toolbox-link" target="_blank" rel="noopener noreferrer">Visit W3Schools.com!</a>}
       leftAvatar={<img src={icon} alt="snail shell icon" height="40" width="40" />}
+      style={{
+        fontSize: '18px',
+        fontWeight: 'bold'
+      }}
     />
   </List>);
 };
