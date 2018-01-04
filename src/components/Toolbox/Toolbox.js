10734ceff2dd2bd37c412004af095adaea8ad137
Lola Brenner <amandadbrenner@gmail.com>
2018-01-04 08:19:49 -0700

    add link placeholders

--- src/components/Toolbox/Toolbox.js
+++ src/components/Toolbox/Toolbox.js
@@ -3,38 +3,35 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import icon from '../../assets/snail-shell.svg';
 import {
-  MobileTearSheet,
   List,
   ListItem,
   Subheader
 } from 'material-ui';
 
 const Toolbox = () => {
-  return (<MobileTearSheet>
-    <List>
-      <Subheader>Mental Health Resources</Subheader>
-      <ListItem
-        primaryText="Brendan Lim"
-        leftAvatar={<img src={icon} alt="snail shell icon" height="40" width="40" />}
-      />
-      <ListItem
-        primaryText="Eric Hoffman"
-        leftAvatar={<img src={icon} alt="snail shell icon" height="40" width="40" />}
-      />
-      <ListItem
-        primaryText="Grace Ng"
-        leftAvatar={<img src={icon} alt="snail shell icon" height="40" width="40" />}
-      />
-      <ListItem
-        primaryText="Kerem Suer"
-        leftAvatar={<img src={icon} alt="snail shell icon" height="40" width="40" />}
-      />
-      <ListItem
-        primaryText="Raquel Parrado"
-        leftAvatar={<img src={icon} alt="snail shell icon" height="40" width="40" />}
-      />
-    </List>
-  </MobileTearSheet>);
+  return (<List>
+    <Subheader>Mental Health Resources</Subheader>
+    <ListItem
+      primaryText={<a href="https://www.w3schools.com" target="_blank" rel="noopener noreferrer">Visit W3Schools.com!</a>}
+      leftAvatar={<img src={icon} alt="snail shell icon" height="40" width="40" />}
+    />
+    <ListItem
+      primaryText=<a href="https://www.w3schools.com" target="_blank" rel="noopener noreferrer">Visit W3Schools.com!</a>
+      leftAvatar={<img src={icon} alt="snail shell icon" height="40" width="40" />}
+    />
+    <ListItem
+      primaryText=<a href="https://www.w3schools.com" target="_blank" rel="noopener noreferrer">Visit W3Schools.com!</a>
+      leftAvatar={<img src={icon} alt="snail shell icon" height="40" width="40" />}
+    />
+    <ListItem
+      primaryText=<a href="https://www.w3schools.com" target="_blank" rel="noopener noreferrer">Visit W3Schools.com!</a>
+      leftAvatar={<img src={icon} alt="snail shell icon" height="40" width="40" />}
+    />
+    <ListItem
+      primaryText=<a href="https://www.w3schools.com" target="_blank" rel="noopener noreferrer">Visit W3Schools.com!</a>
+      leftAvatar={<img src={icon} alt="snail shell icon" height="40" width="40" />}
+    />
+  </List>);
 };
 
 export default Toolbox;
