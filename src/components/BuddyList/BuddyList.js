26a4b8560e15455399ce5ea869c871a94f5f4146
Lola Brenner <amandadbrenner@gmail.com>
2018-01-08 12:10:05 -0700

    add favorite button

--- src/components/BuddyList/BuddyList.js
+++ src/components/BuddyList/BuddyList.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { RaisedButton, FloatingActionButton } from 'material-ui';
-import icon from '../../assets/snail-shell.svg';
+import addHeart from '../../assets/add-heart.png';
+import minusHeart from '../../assets/minus-heart.png';
 
+
+
+// make this component how it would look if everything were open
 const BuddyList = ({ buddies, toggleFavorite, currentUser, faves }) => {
-  const showMore = () => {
+  const toggleView = () => {
     // if not shown
     // show aboutMe and userChallenges
     // change button text to Show Less
@@ -13,24 +17,24 @@ const BuddyList = ({ buddies, toggleFavorite, currentUser, faves }) => {
     // change button text to Show More
   };
 
-  const faveButton = <FloatingActionButton
-    mini={true}
-  >
-    <img src={icon} alt='snail shell' />
-  </FloatingActionButton>;
-
   const buddyStuff = buddies.map((buddy, index) => {
     const isFave = faves.find(fave => fave.userName === buddy.userName);
     return <div key={`${buddy.userName}${index}`}>
       {/* img tag w/ avatar- must send avatar in all user objects */}
-      <p onClick={() => toggleFavorite(currentUser, 'buddy', buddy, isFave)}>{buddy.userName}</p>
-      {faveButton}
+      <p>{buddy.userName}</p>
+
+      <img
+        src={isFave ? addHeart : minusHeart}
+        alt='favorite indicator'
+        onClick={() => toggleFavorite(currentUser, 'buddy', buddy, isFave)}
+      />
+      {/* </FloatingActionButton> */}
       {/* if favorite, render icon button to unfavorite */}
       {/* else, render icon button to favorite */}
       {/* change to icon button */}
-      <RaisedButton label="Show More" onClick={showMore}/>
+      <RaisedButton label="Show Less" onClick={toggleView}/>
       {/* hidden p tag with aboutMe */}
-      {/* hidden Chips from material-ui with userChallenges */}
+      {/* hidden List from material-ui with userChallenges */}
     </div>;
   });
 
