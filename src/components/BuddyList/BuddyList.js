6b99fe911bc43110de8bf0a3fcd7d12c69f4f3cf
Lola Brenner <amandadbrenner@gmail.com>
2018-01-08 13:06:15 -0700

    add snail shell icon

--- src/components/BuddyList/BuddyList.js
+++ src/components/BuddyList/BuddyList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { RaisedButton, FloatingActionButton } from 'material-ui';
+// import { RaisedButton, FloatingActionButton } from 'material-ui';
+import shell from '../../assets/snail-shell.svg';
 import addHeart from '../../assets/add-heart.png';
 import minusHeart from '../../assets/minus-heart.png';
 import minus from '../../assets/minus.png';
@@ -16,11 +17,13 @@ const BuddyList = ({ buddies, toggleFavorite, currentUser, faves }) => {
     // change button text to Show More
   };
 
-  const buddyStuff = buddies.map((buddy, index) => {
+  const buddyStuff = [{ userName: 'lola '}].map((buddy, index) => {
     const isFave = faves.find(fave => fave.userName === buddy.userName);
     return <div key={`${buddy.userName}${index}`}>
       {/* img tag w/ avatar- must send avatar in all user objects */}
-      <p>{buddy.userName}</p>
+      <img src={shell} alt="shell icon" />
+      {/* <p>{buddy.userName}</p> */}
+      <p>LolaKoala</p>
 
       <img
         src={isFave ? addHeart : minusHeart}
@@ -29,13 +32,14 @@ const BuddyList = ({ buddies, toggleFavorite, currentUser, faves }) => {
       />
       <img src={minus} alt="show less" onClick={toggleView} />
       {/* Not going to list their challenges yet because should happen on click of show more */}
-      <p>{buddy.aboutMe}</p>
+      {/* <p>{buddy.aboutMe}</p> */}
+      <p>I am a 32 year old genderqueer person who just started a career as a software developer. I struggle with anxiety daily, and I am happy to talk to anyone about how I experience anxiety and how I survive a typical day.</p>
     </div>;
   });
 
   return (
     <div>
-      <p>Click the heart to add or remove a user from your buddies.</p>
+      <p>Click the heart icon to add or remove a user from your buddies.</p>
       {buddyStuff}
     </div>
   );
